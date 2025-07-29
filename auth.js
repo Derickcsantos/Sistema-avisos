92f59a41b5e615c54c105905a488e3cf6cfdf039
Dérick Campos Santos <164596759+Derickcsantos@users.noreply.github.com>
2025-07-29 11:05:12 -0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,32 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const user = {
+        googleId: profile.id,
+        name: profile.displayName,
+        email: profile.emails[0].value,
+      };
+
+      // Aqui você salva no seu banco de dados, se necessário
+      // ex: await db.saveOrUpdateUser(user)
+
+      return done(null, user);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
